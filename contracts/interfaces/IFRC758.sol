662179a772abe8e949cf4e14a4fd9c2ee33a8bb9
hunter19861024 <frankgreen19881024@gmail.com>
2021-04-21 16:35:18 +0800

    del return params names

--- contracts/interfaces/IFRC758.sol
+++ contracts/interfaces/IFRC758.sol
@@ -5,7 +5,7 @@ pragma solidity = 0.7.5;
 interface IFRC758 {
     event Transfer(address indexed _from, address indexed _to, uint256 amount, uint256 tokenStart, uint256 tokenEnd);
     event ApprovalForAll(address indexed _owner, address indexed _spender, bool _approved);
-    function sliceOf(address _owner) external view returns (uint256[] memory amountArr, uint256[] memory tokenStartArr, uint256[] memory tokenEndArr);
+    function sliceOf(address _owner) external view returns (uint256[] memory, uint256[] memory, uint256[] memory);
     function timeBalanceOf(address _owner, uint256 tokenStart, uint256 tokenEnd) external view returns (uint256);
     function setApprovalForAll(address _spender, bool _approved) external;
     function isApprovedForAll(address _owner, address _spender) external view returns (bool);
