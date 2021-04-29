96eaa897f2a618b0ad94b336b2e194e6af49447c
BuildTools <roderick_lu@163.com>
2021-04-29 18:55:23 +0800

    test

--- test/frc758v2-5.js
+++ test/frc758v2-5.js
@@ -65,7 +65,31 @@ describe("safeTransferFrom", function () {
     const frc758 = await FRC758.deploy("Hello", "HH", 18, 100000000000000);
 
     await frc758.deployed();
-    let now = Date.parse(new Date()) / 1000 +100;
+    const now = Date.parse(new Date()) / 1000 + 1000;
+    const amount = 1000;
+    const amount2 = 2000;
+    await frc758.mintTimeSlice(owner.address, amount, now, now + 100);
+    await frc758.mintTimeSlice(owner.address, amount2, now + 50, now + 200);
+
+    expect(await frc758.timeBalanceOf(owner.address, now, now + 50)).to.equal(amount);
+    expect(await frc758.timeBalanceOf(owner.address, now, now + 100)).to.equal(amount);
+    expect(await frc758.timeBalanceOf(owner.address, now + 50, now + 100)).to.equal(3000);
+
+    await frc758.timeSliceTransferFrom(owner.address, other.address, 1000, now, now + 100); //sub 0-100
+
+    expect(await frc758.timeBalanceOf(owner.address, now, now + 100)).to.equal(0);
+    expect(await frc758.timeBalanceOf(owner.address, now + 50, now + 100)).to.equal(2000);
+    expect(await frc758.timeBalanceOf(other.address, now, now + 50)).to.equal(0);
+    // expect(await frc758.timeBalanceOf(owner.address, now + 50, now + 100)).to.equal(amount);
+  })
+
+  it("transferFrom ", async function () {
+    const [owner, other] = await ethers.getSigners();
+    const FRC758 = await ethers.getContractFactory("ChaingeToken");
+    const frc758 = await FRC758.deploy("Hello", "HH", 18, 100000000000000);
+
+    await frc758.deployed();
+    const now = Date.parse(new Date()) / 1000;
     const basicTime = 30;
     const amount = 100;
     await frc758.mint(owner.address, amount);
@@ -73,16 +97,16 @@ describe("safeTransferFrom", function () {
     // expect(await frc758.timeBalanceOf(owner.address, now, now + 50)).to.equal(amount);
 
     await frc758.timeSliceTransferFrom(owner.address, other.address, 2, now, now + 10000); //sub 0-100
-    expect(await frc758.timeBalanceOf(owner.address, now, now + 99)).to.equal(0);
-    expect(await frc758.timeBalanceOf(owner.address, now, now + 5000)).to.equal(0);
+    expect(await frc758.timeBalanceOf(owner.address, now + basicTime, now + 99)).to.equal(0);
+    expect(await frc758.timeBalanceOf(owner.address, now + basicTime, now + 5000)).to.equal(0);
     expect(await frc758.timeBalanceOf(owner.address, now + 5000, now + 10000)).to.equal(0);
     expect(await frc758.timeBalanceOf(owner.address, now + 5000, '184467440737095551615')).to.equal(0);
     expect(await frc758.timeBalanceOf(owner.address, now + 10000, '18446744073709551615')).to.equal(2);
 
 
     await frc758.timeSliceTransferFrom(owner.address, other.address, 1, now + 5000, '18446744073709551615'); //sub 0-100
-
-    // expect(await frc758.timeBalanceOf(owner.address, now + basicTime, now + 5000)).to.equal(1);
+    // error
+    expect(await frc758.timeBalanceOf(owner.address, now + basicTime, now + 5000)).to.equal(1);
     // console.log(now);
     // expect(await frc758.timeBalanceOf(owner.address, now + basicTime, now + basicTime + 99)).to.equal(1);
     // expect(await frc758.timeBalanceOf(owner.address, now + basicTime + 5000, '18446744073709551615')).to.equal(0);
@@ -97,7 +121,9 @@ describe("safeTransferFrom", function () {
     // expect(await frc758.timeBalanceOf(owner.address, now + basicTime + 100, '18446744073709551615')).to.equal(0);
 
 
+    // console.log('当前时间:', now+99, now+100);
     // expect(await frc758.timeBalanceOf(other.address, now + 99, now + 100)).to.equal(0);
+
 
     const res = await frc758.sliceOf(owner.address)
     const _res = res.map((val) => {
@@ -105,6 +131,9 @@ describe("safeTransferFrom", function () {
         return parseInt(v._hex)
       })
     })
+
+    // console.log(_res);
+    // const count = _res[0].length;
 
     const format = []
     for (let k in _res[0]) {
